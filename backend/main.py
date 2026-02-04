b9c63d0b371a38711bf6aa933dbb0e550b1b5cab
Jean <jctorres4193@gmail.com>
2026-02-04 01:05:34 -0500

    Added an endpoint to test the backend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_restful import Api
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/data", methods=['GET'])
+def get_data():
+    test_message = "This endpoint just sent you a message"
+
+    return jsonify({"message": test_message})
+
+if __name__ == "__main__":
+    app.run(debug=True)
